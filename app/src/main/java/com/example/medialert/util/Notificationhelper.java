1c736154974ab879ba37b8d79b4c51bf2e59ccad
nullpointerexpected <juniorprogrammer09@gmail.com>
2024-04-04 13:55:34 +0300

    modification

--- app/src/main/java/com/example/medialert/util/Notificationhelper.java
+++ app/src/main/java/com/example/medialert/util/Notificationhelper.java
@@ -22,7 +22,7 @@ public class Notificationhelper {
     private static final int NOTIFICATIONS_ID = 123;
     private static final int ALARM_REQUEST_CODE = 100;
 
-    public static void scheduleNotification(Context context, Reminder reminder){
+    public static void scheduleNotification(Context context, Reminder reminder) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.YEAR, 2024);
         calendar.set(Calendar.HOUR_OF_DAY, reminder.getTimestamp().getHour());
@@ -30,25 +30,27 @@ public class Notificationhelper {
         calendar.set(Calendar.SECOND, 0);
 
         Intent intent = new Intent(context, AlarmReciever.class);
+        intent.putExtra("rid", reminder.getId());
         intent.putExtra("title", reminder.getMedicineName());
-        intent.putExtra("message", "Reminder for "+reminder.getMedicineName());
+        intent.putExtra("message", String.format("please remember to take the dose of %s at %d %s", reminder.getMedicineName(), reminder.getDose().getDose(), reminder.getDose().getForm()));
 
 
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, ALARM_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
 
-        if (alarmManager != null){
+        if (alarmManager != null) {
             alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
         }
 
     }
 
-    public static void showNotification(Context context, String title,String content){
+    public static void showNotification(Context context, String title, String content) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         Intent intent = new Intent(context, HomeScreen.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 ,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+	intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP); // Ensure only one instance of HomeScreen
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
@@ -58,11 +60,11 @@ public class Notificationhelper {
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
             notificationManager.createNotificationChannel(channel);
         }
-
+	builder.setPriority(NotificationCompat.PRIORITY_HIGH);
         notificationManager.notify(NOTIFICATIONS_ID, builder.build());
     }
 }
